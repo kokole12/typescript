760f93e1524981e0b9e36a5a22aeaf0b2dd3a30a
kokole12 <kokoleismail886@gmail.com>
2023-06-12 11:57:49 +0300

    ts objects

---
+++ objects/0-type.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: "kokole",
+    email: "kokole@gmail.com",
+    isActive: true
+};
+function creatUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+function creatUsers() {
+    return { name: "kokole", isPaid: false };
+}
---
+++ objects/0-type.ts
@@ -0,0 +1,17 @@
+const user = {
+    name: "kokole",
+    email: "kokole@gmail.com",
+    isActive: true
+
+}
+
+
+function creatUser({name: string, isPaid: boolean}){
+
+}
+
+function creatUsers(): {name: string, isPaid: boolean} {
+    return {name:"kokole", isPaid: false};
+}
+
+export {};
