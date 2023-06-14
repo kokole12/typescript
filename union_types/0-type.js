cf705206fce9fa596ec193ef25535ed02bfa1c1b
kokole12 <kokoleismail886@gmail.com>
2023-06-14 21:56:04 +0300

    union types

---
+++ union_types/0-type.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var score = 33;
+score = "44";
+score = 55;
--- union_types/0-type.ts
+++ union_types/0-type.ts
@@ -1,1 +1,7 @@
 let score: number | string = 33;
+
+score = "44";
+
+score = 55;
+
+export {};
