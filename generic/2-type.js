dc8445f013245117bb7bfd5f7857a24bfe937093
kokole12 <kokoleismail886@gmail.com>
2023-06-17 22:00:19 +0300

    generic types

---
+++ generic/2-type.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getScores(scores) {
+    var myIndex = 3;
+    return scores[myIndex];
+}
+var searchScore = function (scores) {
+    var myIndex = 3;
+    return scores[myIndex];
+};
---
+++ generic/2-type.ts
@@ -0,0 +1,11 @@
+function getScores<T>(scores: T[]): T {
+    const myIndex = 3;
+    return scores[myIndex];
+}
+
+const searchScore = <T>(scores: T[]): T =>{
+    const myIndex = 3
+    return scores[myIndex]
+}
+
+export {};
